Fixed SDF scaling

utilities/point_transform.py
@@ -44,7 +44,7 @@ def quats_to_rot_matrices(quaternions):
 # Transforms a BxNx3 point cloud tensor to a given space
 # Where B is the number of point clouds and N is the number of points in each cloud
 # Target space is defined by a Bx3 translation tensor, a Bx4 quaternion tensor, and a Bx3 scale tensor
-def transform_point_clouds(point_clouds, translations, rotations, scales):
+def transform_point_clouds(point_clouds, translations, rotations):
 	B = point_clouds.size(dim=0)
 
 	# Translate points
@@ -56,9 +56,6 @@ def transform_point_clouds(point_clouds, translations, rotations, scales):
 	transformed_points = transformed_points.unsqueeze(-1)
 	transformed_points = rot_matrices.matmul(transformed_points).squeeze(-1)
 
-	# Scale points
-	transformed_points /= scales
-
 	return transformed_points
 
 
@@ -67,12 +64,11 @@ if __name__ == "__main__":
 	batch_size = 2
 	num_points = 2
 
-	points = torch.randn([batch_size, num_points, 3])
+	points = torch.rand([batch_size, num_points, 3])
 	translations = torch.randn([batch_size, 3])
 	rotations = torch.randn([batch_size, 4])
-	scales = torch.tensor([0.5]).repeat(batch_size, 3)
 
 	rotations = torch.nn.functional.normalize(rotations, p=2, dim=-1)
 
 	print('Transformed Points:')
-	print(transform_point_clouds(points, translations, rotations, scales))
+	print(transform_point_clouds(points, translations, rotations))

utilities/sdf_primitives.py
@@ -1,31 +1,27 @@
 import torch
 from point_transform import transform_point_clouds
 
-import trimesh
-import pyrender
-import numpy as np
-
 
 # Equations for all primitive SDFs are borrowed from iquilezles.org
 # https://iquilezles.org/articles/distfunctions/
 
 
-def sdf_sphere(query_points, translations, rotations, scales):
-	radius = 1
-
+def sdf_ellipsoid(query_points, translations, rotations, dimensions):
 	# Transform query points to primitive space
-	transformed_query_points = transform_point_clouds(query_points, translations, rotations, scales)
-	# Find distance to surface of sphere
-	distances = transformed_query_points.norm(dim=-1) - radius
+	transformed_query_points = transform_point_clouds(query_points, translations, rotations)
+
+	# Scale sphere to approximate ellipsoid
+	k0 = (transformed_query_points / dimensions).norm(dim=-1)
+	# Divide by gradient to minimize distortion
+	k1 = (transformed_query_points / (dimensions*dimensions)).norm(dim=-1)
+	distances = k0 * (k0 - torch.ones_like(k0)) / k1
 
 	return distances
 
 
-def sdf_cuboid(query_points, translations, rotations, scales):
-	dimensions = torch.tensor([1,1,1], dtype=float).unsqueeze(0)
-
+def sdf_cuboid(query_points, translations, rotations, dimensions):
 	# Transform query points to primitive space
-	transformed_query_points = transform_point_clouds(query_points, translations, rotations, scales)
+	transformed_query_points = transform_point_clouds(query_points, translations, rotations)
 
 	# Reflect query point in all quadrants and translate relative to the box surface
 	transformed_query_points = transformed_query_points.abs() - dimensions
@@ -42,24 +38,29 @@ def sdf_cuboid(query_points, translations, rotations, scales):
 	return pos_distance + neg_distance
 
 
-def sdf_cylinder(query_points, translations, rotations, scales):
-	dimensions = torch.tensor([1,1], dtype=float).unsqueeze(0)
-
+def sdf_cylinder(query_points, translations, rotations, dimensions):
 	# Transform query points to primitive space
-	transformed_query_points = transform_point_clouds(query_points, translations, rotations, scales)
+	transformed_query_points = transform_point_clouds(query_points, translations, rotations)
 
-	# Compute distance to uncapped cylinder
 	qxy = transformed_query_points[..., :2]
 	qz = transformed_query_points[..., 2]
+	sxy = dimensions[..., :2]
+	sz = dimensions[..., 2]
 
-	length = qxy.norm(dim=-1)
-	d = torch.stack((length, qz), dim=-1).abs()
-	d -= dimensions
+	# Scale circle to approximate distance to ellipse
+	k0 = (qxy / sxy).norm(dim=-1)
+	# Divide by gradient to minimize distortion
+	k1 = (qxy / (sxy*sxy)).norm(dim=-1)
+	ellipse_distance = k0 * (k0 - torch.ones_like(k0)) / k1
 
-	dx = d[..., 0]
-	dy = d[..., 1]
+	# Compute distance to ends of cylinder
+	cap_distance = qz.abs() - sz
 
-	pos_distance = torch.max(d, torch.zeros_like(d)).norm(dim=-1)
+	distance = torch.stack((ellipse_distance, cap_distance), dim=-1)
+	dx = distance[..., 0]
+	dy = distance[..., 1]
+
+	pos_distance = torch.max(distance, torch.zeros_like(distance)).norm(dim=-1)
 	neg_distance = torch.min(torch.max(dx, dy), torch.zeros_like(dx))
 
 	return pos_distance + neg_distance
@@ -70,12 +71,12 @@ if __name__ == "__main__":
 	batch_size = 2
 	num_points = 2
 
-	points = torch.randn([batch_size, num_points, 3])
+	points = torch.rand([batch_size, num_points, 3])
 	translations = torch.tensor([0,0.2,0], dtype=float).unsqueeze(0)
 	rotations = torch.tensor([0.924,0,0,0.383], dtype=float).unsqueeze(0)
-	scales = torch.tensor([0.5,0.5,0.5], dtype=float).unsqueeze(0)
+	scales = torch.tensor([0.2,0.5,0.7], dtype=float).unsqueeze(0)
 
-	distances = sdf_sphere(points, translations, rotations, scales)
+	distances = sdf_ellipsoid(points, translations, rotations, scales)
 	print('Sphere SDF Samples:')
 	print(distances, '\n')
 