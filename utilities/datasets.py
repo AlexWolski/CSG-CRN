a45bc5474303405b41493158957f82e387d4bf91
Alex <alex.theinfinite@gmail.com>
2022-12-07 03:16:03 -0800

    Removed uniform samples from dataset

--- utilities/datasets.py
+++ utilities/datasets.py
@@ -5,9 +5,8 @@ from torch.utils.data import Dataset
 
 
 class PointDataset(Dataset):
-	def __init__(self, uniform_dir, surface_dir, filenames, num_points, device):
-		self.uniform_dir = uniform_dir
-		self.surface_dir = surface_dir
+	def __init__(self, data_dir, filenames, num_points, device):
+		self.data_dir = data_dir
 		self.filenames = filenames
 		self.num_points = num_points
 		self.device = device
@@ -16,23 +15,17 @@ class PointDataset(Dataset):
 		return len(self.filenames)
 
 	def __getitem__(self, idx):
+		# Load all points from file
 		filename = self.filenames[idx]
-
-		# Load uniform points from file
-		uniform_path = os.path.join(self.uniform_dir, filename)
-		uniform_points = np.load(uniform_path)
-
-		# Load surface points from file
-		surface_path = os.path.join(self.surface_dir, filename)
-		surface_points = np.load(surface_path)
+		points_path = os.path.join(self.data_dir, filename)
+		points = np.load(points_path)
 
 		# Randomly select needed number of surface points
-		replace = (surface_points.shape[0] < self.num_points)
-		select_rows = np.random.choice(surface_points.shape[0], self.num_points, replace=replace)
-		surface_points = surface_points[select_rows]
+		replace = (points.shape[0] < self.num_points)
+		select_rows = np.random.choice(points.shape[0], self.num_points, replace=replace)
+		select_points = points[select_rows]
 
 		# Convert numpy arrays to torch tensors
-		uniform_points = torch.from_numpy(uniform_points)
-		surface_points = torch.from_numpy(surface_points)
+		select_points = torch.from_numpy(points)
 
-		return (uniform_points, surface_points)
+		return (points, select_points)
