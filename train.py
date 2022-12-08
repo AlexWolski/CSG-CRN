12b6c975a6bd26d931b7a5b3633d04cf0d4f376a
Alex <alex.theinfinite@gmail.com>
2022-12-07 20:57:20 -0800

    Adjusted default primitives and iterations settings

--- train.py
+++ train.py
@@ -36,8 +36,8 @@ def options():
 	# Model settings
 	parser.add_argument('--num_input_points', type=int, default=1024, help='Number of points in the input point clouds')
 	parser.add_argument('--num_loss_points', type=int, default=20000, help='Number of points to use in computing the loss')
-	parser.add_argument('--num_prims', type=int, default=10, help='Number of primitives to generate each iteration')
-	parser.add_argument('--num_iters', type=int, default=5, help='Number refinement iterations to train for')
+	parser.add_argument('--num_prims', type=int, default=3, help='Number of primitives to generate before computing loss')
+	parser.add_argument('--num_iters', type=int, default=10, help='Number of refinement iterations to train for (Total primitives = num_prims x num_iters)')
 	parser.add_argument('--clamp_dist', type=float, default=0.1, help='SDF clamping value for computing reconstruciton loss (Recommended to set clamp_dist to sample_dist)')
 	parser.add_argument('--batch_size', type=int, default=64, help='Mini-batch size')
 	parser.add_argument('--max_epochs', type=int, default=2000, help='Maximum number of epochs to train')
