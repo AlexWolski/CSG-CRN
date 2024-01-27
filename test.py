39161e3a68e5066e3685b4ab12ada4aaeff2e796
Alex Wolski <alex.theinfinite@gmail.com>
2024-01-27 15:05:52 -0800

    Added all loss types to test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-from losses.primitive_loss import test
-test()
+from losses import primitive_loss
+from losses import entropy_loss
+from losses import reconstruction_loss
+
+primitive_loss.test()
+entropy_loss.test()
+reconstruction_loss.test()
