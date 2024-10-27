a33b5cd95a55c0ffe9905b531577de6ea005f9bc
Alex Wolski <alex.theinfinite@gmail.com>
2024-10-27 14:35:32 -0700

    Added parent RegressorNetwork class to generalize regressor networks

--- networks/regressor_decoder.py
+++ networks/regressor_decoder.py
@@ -15,217 +15,112 @@ DEFAULT_MIN_BLENDING = 0.001
 DEFAULT_MAX_BLENDING = 1
 
 
-# Predict probability distribution for output shape primitive
-class ShapeRegressor(nn.Module):
-	def __init__(self, num_shapes):
-		super(ShapeRegressor, self).__init__()
-		self.num_shapes = num_shapes
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.num_shapes))
-
+# Parent regressor network class to generalize network building
+class RegressorNetwork(nn.Module):
+	def __init__(self, layer_sizes, activation_func=None):
+		super(RegressorNetwork, self).__init__()
+		self.layer_sizes = layer_sizes
+		self.activation_func = activation_func
 		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = nn.Softmax(dim=-1)
+		self.init_layers();
+
+	def init_layers(self):
+		self.fc_list = nn.ModuleList()
+
+		for i in range(len(self.layer_sizes) - 1):
+			self.fc_list.append(nn.Linear(self.layer_sizes[i], self.layer_sizes[i+1]))
 
 	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
+		for i in range(len(self.fc_list)):
+			fc_layer = self.fc_list[i]
+			X = fc_layer(X)
 
-			if i < self.num_layers - 1:
+			if i < len(self.fc_list) - 1:
 				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
+			elif self.activation_func != None:
+				X = self.activation_func(X)
 
 		return X
+
+
+# Predict probability distribution for output shape primitive
+class ShapeRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes, num_shapes):
+		layer_sizes.append(num_shapes)
+		activation = nn.Softmax(dim=-1)
+		super(ShapeRegressor, self).__init__(layer_sizes, activation)
 
 
 # Predict probability distribution for boolean operation to apply
-class OperationRegressor(nn.Module):
-	def __init__(self, num_operations):
-		super(OperationRegressor, self).__init__()
-		self.num_operations = num_operations
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.num_operations))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = nn.Softmax(dim=-1)
-
-	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
-
-			if i < self.num_layers - 1:
-				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
-
-		return X
+class OperationRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes, num_operations):
+		layer_sizes.append(num_operations)
+		activation = nn.Softmax(dim=-1)
+		super(OperationRegressor, self).__init__(layer_sizes, activation)
 
 
 # Predict 3D coordinate
-class TranslationRegressor(nn.Module):
-	def __init__(self, translation_scale):
-		super(TranslationRegressor, self).__init__()
+class TranslationRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes, translation_scale):
 		self.translation_scale = translation_scale
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-		self.output_size = 3
+		layer_sizes.append(3)
+		activation = nn.Tanh()
+		super(TranslationRegressor, self).__init__(layer_sizes, activation)
 
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.output_size))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = nn.Tanh()
-
+	# Restrict predicted coordinates to fit the output unit cube
 	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
-
-			if i < self.num_layers - 1:
-				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
-
-		# Restrict predicted coordinates to fit the output unit cube
-		translation = X * self.translation_scale
-
-		return translation
+		translation = super(TranslationRegressor, self).forward(X)
+		return translation * self.translation_scale
 
 
 # Predict quaternion rotation
-class RotationRegressor(nn.Module):
-	def __init__(self):
-		super(RotationRegressor, self).__init__()
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-		self.output_size = 4
+class RotationRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes):
+		layer_sizes.append(4)
+		super(RotationRegressor, self).__init__(layer_sizes)
 
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.output_size))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-
+	# Normalize quaternion
 	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = self.LeReLU(fc(X))
-
-		# Normalize quaternion
-		quaternion = torch.nn.functional.normalize(X, dim=-1)
-		return quaternion
+		quaternion = super(RotationRegressor, self).forward(X)
+		return torch.nn.functional.normalize(X, dim=-1)
 
 
 # Predict shape scale in x, y, and z axes
-class ScaleRegressor(nn.Module):
-	def __init__(self, min_scale, max_scale):
-		super(ScaleRegressor, self).__init__()
+class ScaleRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes, min_scale, max_scale):
 		self.min_scale = min_scale
 		self.max_scale = max_scale
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-		self.output_size = 3
+		layer_sizes.append(3)
+		activation = torch.sigmoid
+		super(ScaleRegressor, self).__init__(layer_sizes, activation)
 
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.output_size))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = torch.sigmoid
-
+	# Restrict the predicted scale to expected range
 	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
-
-			if i < self.num_layers - 1:
-				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
-
-		# Restrict the predicted scale to expected range
-		scale = (X * (self.max_scale - self.min_scale)) + self.min_scale
-
-		return scale
+		scale = super(ScaleRegressor, self).forward(X)
+		return (scale * (self.max_scale - self.min_scale)) + self.min_scale
 
 
 # Predict amount to blend output primitive with current reconstruction
-class BlendingRegressor(nn.Module):
-	def __init__(self, min_blending, max_blending):
-		super(BlendingRegressor, self).__init__()
+class BlendingRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes, min_blending, max_blending):
 		self.min_blending = min_blending
 		self.max_blending = max_blending
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-		self.output_size = 1
-
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.output_size))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = torch.sigmoid
+		layer_sizes.append(1)
+		activation = torch.sigmoid
+		super(BlendingRegressor, self).__init__(layer_sizes, activation)
 	
+	# Restrict the blending factor to expected range
 	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
-
-			if i < self.num_layers - 1:
-				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
-
-		# Restrict the blending factor to expected range
-		blending = (X * (self.max_blending - self.min_blending)) + self.min_blending
-
-		return blending
+		blending = super(BlendingRegressor, self).forward(X)
+		return (blending * (self.max_blending - self.min_blending)) + self.min_blending
 
 
 # Predict amount to round shape into sphere
-class RoundnessRegressor(nn.Module):
-	def __init__(self):
-		super(RoundnessRegressor, self).__init__()
-		self.fc_list = nn.ModuleList()
-		self.num_layers = len(REGRESSOR_LAYER_SIZES)
-		self.output_size = 1
-
-		for i in range(self.num_layers - 1):
-			self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[i], REGRESSOR_LAYER_SIZES[i+1]))
-
-		self.fc_list.append(nn.Linear(REGRESSOR_LAYER_SIZES[-1], self.output_size))
-
-		self.LeReLU = nn.LeakyReLU(LEAKY_RELU_NEGATIVE_SLOPE, True)
-		self.activation = torch.sigmoid
-	
-	def forward(self, X):
-		for i in range(self.num_layers):
-			fc = self.fc_list[i]
-			X = fc(X)
-
-			if i < self.num_layers - 1:
-				X = self.LeReLU(X)
-			else:
-				X = self.activation(X)
-
-		return X
+class RoundnessRegressor(RegressorNetwork):
+	def __init__(self, layer_sizes):
+		layer_sizes.append(1)
+		activation = torch.sigmoid
+		super(RoundnessRegressor, self).__init__(layer_sizes, activation)
 
 
 # Pridict all primitive parameters 
@@ -242,13 +137,13 @@ class PrimitiveRegressor(nn.Module):
 
 		super(PrimitiveRegressor, self).__init__()
 
-		self.shape = ShapeRegressor(num_shapes)
-		self.operation = OperationRegressor(num_operations)
-		self.translation = TranslationRegressor(translation_scale)
-		self.rotation = RotationRegressor()
-		self.scale = ScaleRegressor(min_scale, max_scale)
-		self.blending = BlendingRegressor(min_blending, max_blending) if (predict_blending) else (None)
-		self.roundness = RoundnessRegressor() if (predict_roundness) else (None)
+		self.shape = ShapeRegressor(REGRESSOR_LAYER_SIZES, num_shapes)
+		self.operation = OperationRegressor(REGRESSOR_LAYER_SIZES, num_operations)
+		self.translation = TranslationRegressor(REGRESSOR_LAYER_SIZES, translation_scale)
+		self.rotation = RotationRegressor(REGRESSOR_LAYER_SIZES)
+		self.scale = ScaleRegressor(REGRESSOR_LAYER_SIZES, min_scale, max_scale)
+		self.blending = BlendingRegressor(REGRESSOR_LAYER_SIZES, min_blending, max_blending) if (predict_blending) else (None)
+		self.roundness = RoundnessRegressor(REGRESSOR_LAYER_SIZES) if (predict_roundness) else (None)
 	
 	def forward(self, X, has_initial_recon):
 		shape = self.shape.forward(X)
@@ -276,41 +171,41 @@ def test():
 	feature_size = 256
 	inputs = torch.autograd.Variable(torch.rand(batch_size, feature_size))
 
-	network = ShapeRegressor(3)
+	network = ShapeRegressor(REGRESSOR_LAYER_SIZES, 3)
 	outputs = network(inputs)
 	print('Shape Output:')
 	print(outputs, '\n')
 	
-	network = OperationRegressor(2)
+	network = OperationRegressor(REGRESSOR_LAYER_SIZES, 2)
 	outputs = network(inputs)
 	print('Boolean Regressor Output:')
 	print(outputs, '\n')
 	
-	network = TranslationRegressor(0.6)
+	network = TranslationRegressor(REGRESSOR_LAYER_SIZES, 0.6)
 	outputs = network(inputs)
 	print('Translation Output:')
 	print(outputs, '\n')
 	
-	network = RotationRegressor()
+	network = RotationRegressor(REGRESSOR_LAYER_SIZES)
 	outputs = network(inputs)
 	print('Rotation Output:')
 	print(outputs, '\n')
 
-	network = ScaleRegressor(0.005, 0.5)
+	network = ScaleRegressor(REGRESSOR_LAYER_SIZES, 0.005, 0.5)
 	outputs = network(inputs)
 	print('Scale Output:')
 	print(outputs, '\n')
 
-	network = BlendingRegressor(0, 1)
+	network = BlendingRegressor(REGRESSOR_LAYER_SIZES, 0, 1)
 	outputs = network(inputs)
 	print('Blending Output:')
 	print(outputs, '\n')
 
-	network = RoundnessRegressor()
+	network = RoundnessRegressor(REGRESSOR_LAYER_SIZES)
 	outputs = network(inputs)
 	print('Roundness Output:')
 	print(outputs, '\n')
 
-	network = PrimitiveRegressor(3, 2)
+	network = PrimitiveRegressor(REGRESSOR_LAYER_SIZES, 3, 2)
 	outputs = network(inputs)
 	print('Combined Output Size:', len(outputs))
