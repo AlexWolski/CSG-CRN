82afad4d80be2de1cb570bcb5fb632ab4742e37a
Alex Wolski <alex.theinfinite@gmail.com>
2024-10-31 23:08:20 -0700

    Modified datasets.py to load all data samples into RAM during construction to speed up retrieval

--- utilities/datasets.py
+++ utilities/datasets.py
@@ -1,36 +1,41 @@
 import os
 import numpy as np
 import torch
+from tqdm import tqdm
 from torch.utils.data import Dataset
 from utilities.data_augmentation import augment_sample_batch
 
 
 class PointDataset(Dataset):
 	def __init__(self, file_rel_paths, device, args):
-		self.file_rel_paths = file_rel_paths
+
 		self.raw_copies = len(file_rel_paths)
 		self.device = device
 		self.augmented_copies = len(file_rel_paths) * args.augment_copies
 		self.args = args
+		self.sdf_sample_list = []
+
+		# Load all data samples into memory
+		for file_rel_path in tqdm(file_rel_paths, desc="Loading Dataset"):
+			sample_path = os.path.join(self.args.data_dir, file_rel_path)
+			sdf_sample = np.load(sample_path).astype(np.float32)
+			sdf_sample = torch.from_numpy(sdf_sample)
+			self.sdf_sample_list.append(sdf_sample)
 
 	def __len__(self):
 		return self.augmented_copies
 
 	def __getitem__(self, batch_idx):
-		sdf_samples_list = []
+		batch_samples_list = []
 
 		# Load all points and distances from sdf sample file
 		for idx in batch_idx:
 			index = idx % self.raw_copies
-			total_points = self.args.num_input_points + self.args.num_loss_points
-			file_rel_path = self.file_rel_paths[index]
-			sample_path = os.path.join(self.args.data_dir, file_rel_path)
-			sdf_sample = np.load(sample_path).astype(np.float32)
-			sdf_sample = torch.from_numpy(sdf_sample).to(self.device)
-			sdf_samples_list.append(sdf_sample)
+			sdf_sample = self.sdf_sample_list[index].to(self.device)
+			batch_samples_list.append(sdf_sample)
 
 		# Combine loaded samples into batch
-		batch_sdf_samples = torch.stack(sdf_samples_list, dim=0)
+		batch_sdf_samples = torch.stack(batch_samples_list, dim=0)
 
 		# Augment samples
 		if self.args.augment_data:
@@ -42,6 +47,7 @@ class PointDataset(Dataset):
 			batch_sdf_samples = torch.cat((augmented_points, augmented_distances), dim=2)
 
 		# Shuffle the data samples
+		total_points = self.args.num_input_points + self.args.num_loss_points
 		batch_sdf_samples = batch_sdf_samples[:, torch.randperm(total_points)]
 
 		# Separate input and loss samples
