eedecebdf90bc44d0675a53442d460032445fa57
Alex Wolski <alex.theinfinite@gmail.com>
2024-11-26 12:16:52 -0800

    Fixed negative sign bug in subtract_sdf function

--- utilities/csg_model.py
+++ utilities/csg_model.py
@@ -38,7 +38,7 @@ def add_sdf(distances, new_distances, blending):
 
 # Intersection of one SDF and the conjugate of the other
 def subtract_sdf(distances, new_distances, blending):
-	return smooth_max(distances, new_distances, blending)
+	return smooth_max(distances, -new_distances, blending)
 
 
 # Map primitive SDF function to MagicaCSG primitive name
