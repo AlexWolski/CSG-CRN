828c52b873a4987134add5623888a442b9716fae
Alex Wolski <alex.theinfinite@gmail.com>
2026-08-16 15:19:40 -0700

    Added support for parsing device lists

--- utilities/device_utils.py
+++ utilities/device_utils.py
@@ -24,6 +24,33 @@ def get_device(device=None, cpu_allowed=False):
 		raise Exception(f'Device {device} does not exist. Select a device: ' + get_available_devices(cpu_allowed))
 
 
+# Determine list of devices to train on
+def get_devices(devices=None, cpu_allowed=False):
+	# When no devices are not specified, automatically select one
+	if not devices:
+		return [get_device(None, cpu_allowed)]
+
+	# Convert string parameter to list
+	if isinstance(devices, str):
+		devices = [devices]
+
+	resolved_devices = []
+	has_cpu_device = False
+	has_cuda_device = False
+
+	# Resolve all devices
+	for device in devices:
+		resolved_devices.push(get_device(device, cpu_allowed))
+		has_cpu_device = True if device == 'cpu' else has_cpu_device
+		has_cuda_device = True if device.startswith('cuda') else has_cuda_device
+
+	# Check for invalid comnbinations
+	if has_cpu_device and has_cuda_device:
+		raise Exception('Cannot use both CPU and CUDA devices together')
+
+	return resolved_devices
+
+
 # Return a list of available training devices
 def get_available_devices(cpu_allowed):
 	if not torch.cuda.is_available() and not cpu_allowed:
