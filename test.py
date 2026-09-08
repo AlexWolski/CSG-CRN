a103fe11fd32b2e748a27a29653f4c97d2d81caf
Alex Wolski <alex.theinfinite@gmail.com>
2026-09-07 20:26:49 -0700

    Added earth movers distance metric to test.py

--- test.py
+++ test.py
@@ -11,10 +11,13 @@ import torch
 import torch.multiprocessing as mp
 from tqdm import tqdm
 from wakepy import keep
-from reconstruct import compute_chamfer_distance_mesh, compute_recon_loss, load_mesh_and_samples, load_model, model_inference
+from losses.reconstruction_loss import ReconstructionLoss
+from reconstruct import load_mesh_and_samples, load_model, model_inference
+from utilities.accuracy_metrics import EMD, compute_chamfer_distance
 from utilities.csg_to_mesh import csg_to_mesh
 from utilities.data_processing import BEST_MODEL_FILE, find_file_paths, get_test_set
 from utilities.device_utils import get_devices
+from utilities.sampler_utils import sample_points_mesh_surface
 
 
 # Parse commandline arguments
@@ -26,7 +29,7 @@ def options():
 	parser.add_argument('--model_params', type=str, help='Path to a trained model pytorch file. Only needed when train_output_path is not provided.')
 	parser.add_argument('--test_set_path', type=str, help='Path to a directory containing mesh files to test on. Overwrites the saved test set when train_output_path is also provided.')
 	parser.add_argument('--num_cascades', type=int, help='Number of cascades to output before running tests. Defaults to the maximum number of cascades used during training.')
-	parser.add_argument('--num_acc_points', type=int, default=30000, help='Number of points to use when computing accuracy.')
+	parser.add_argument('--num_acc_points', type=int, default=10000, help='Number of points to use when computing accuracy.')
 	parser.add_argument('--recon_resolution', type=int, default=512, help='Voxel resolution to use for the marching cubes algorithm when computing accuracy.')
 	parser.add_argument('--device', type=str.lower, default=[], nargs='*', help='Select one or more devices. CPU and GPU devices cannot be mixed. Select "all" to use all available cuda devices.')
 
@@ -64,6 +67,7 @@ def test_worker(worker_index, devices, model_params, num_acc_points, num_cascade
 
 	summed_recon_loss = 0.0
 	summed_chamfer_dist = 0.0
+	summed_earth_dist = 0.0
 
 	for sample_file in tqdm(test_samples, desc=str(device), position=worker_index):
 		# Reconstruct sample and generate a mesh.
@@ -71,11 +75,19 @@ def test_worker(worker_index, devices, model_params, num_acc_points, num_cascade
 		csg_model = model_inference(model, saved_args, init_model_state_dict, prev_cascades_list, near_surface_samples, uniform_samples)
 		recon_mesh = csg_to_mesh(csg_model, recon_resolution)[0]
 
-		# Compute and accumulate the test metrics.
-		summed_recon_loss += compute_recon_loss(near_surface_samples, uniform_samples, surface_points, csg_model, saved_args.loss_metric, saved_args.excess_loss_weight).item()
-		summed_chamfer_dist += compute_chamfer_distance_mesh(target_mesh, recon_mesh, num_acc_points, device)
+		# Compute reconstruction loss with original training settings.
+		recon_loss = ReconstructionLoss(saved_args.loss_metric, saved_args.excess_loss_weight)
+		summed_recon_loss += recon_loss.forward(near_surface_samples, uniform_samples, surface_points, csg_model)
 
-	result_queue.put((summed_recon_loss, summed_chamfer_dist))
+		# Sample points from output mesh.
+		target_points = sample_points_mesh_surface(target_mesh, num_acc_points).unsqueeze(0).to(device)
+		recon_points = sample_points_mesh_surface(recon_mesh, num_acc_points).unsqueeze(0).to(device)
+
+		# Compute accuracy metrics.
+		summed_chamfer_dist += compute_chamfer_distance(target_points, recon_points, no_grad=True)
+		summed_earth_dist += EMD(target_points[0], recon_points[0]).item()
+
+	result_queue.put((summed_recon_loss, summed_chamfer_dist, summed_earth_dist))
 
 
 def test(model_params, num_acc_points, num_cascades, recon_resolution, devices, test_samples):
@@ -101,14 +113,16 @@ def test(model_params, num_acc_points, num_cascades, recon_resolution, devices, 
 	total_chamfer_dist = 0
 
 	for _ in range(num_workers):
-		recon_loss, chamfer_dist = result_queue.get()
+		recon_loss, chamfer_dist, earth_dist = result_queue.get()
 		total_recon_loss += recon_loss
 		total_chamfer_dist += chamfer_dist
+		total_earth_dist += earth_dist
 
 	mean_recon_loss = total_recon_loss / num_samples
 	mean_chamfer_dist = total_chamfer_dist / num_samples
+	mean_earth_dist = total_earth_dist / num_samples
 
-	return (mean_recon_loss, mean_chamfer_dist)
+	return (mean_recon_loss, mean_chamfer_dist, mean_earth_dist)
 
 
 def main():
@@ -129,10 +143,11 @@ def main():
 		test_set_paths = [str(path.resolve()) for path in target_dir.rglob('*') if path.is_file()]
 
 	# Test model.
-	mean_recon_loss, mean_chamfer_dist = test(args.model_params, args.num_acc_points, args.num_cascades, args.recon_resolution, devices, test_set_paths)
+	mean_recon_loss, mean_chamfer_dist, mean_earth_dist = test(args.model_params, args.num_acc_points, args.num_cascades, args.recon_resolution, devices, test_set_paths)
 
 	print(f'Reconstruction Loss: {mean_recon_loss}')
 	print(f'Chamfer Distance: {mean_chamfer_dist}')
+	print(f'Earth Movers Distance: {mean_earth_dist}')
 
 
 if __name__ == '__main__':
--- utilities/accuracy_metrics.py
+++ utilities/accuracy_metrics.py
@@ -85,3 +85,122 @@ def compute_chamfer_distance_csg_fast(target_surface_samples, csg_model, num_acc
 	(recon_points_batch, _) = sample_sdf_near_csg_surface(csg_model, num_acc_points, sample_dist)
 	# Compute average Chamfer distance
 	return compute_chamfer_distance(target_surface_samples, recon_points_batch)
+
+
+########################################################################################################
+## Earth Movers Distance Implementation from 3D Point Cloud Modeling Paper (2017, Achlioptas et. al.) ##
+########################################################################################################
+
+
+def approxMatch(X, Y):
+    """
+    Calculates Approximate Matching. An iterative algorithm to calculate 
+    matching matrix.
+
+    Parameter : 
+    X (torch.tensor) : 3D point cloud of dimension (2048,3)
+    Y (torch.tensor) : 3D point cloud of dimension (2048,3)
+
+    Returns:
+    (torch.tensor) : A matching matrix of dimension (2048, 2048) 
+    """
+
+    n = X.shape[0]
+    m = Y.shape[0]
+    factorl = max(n,m)/n
+    factorr = max(n,m)/m
+
+    device = X.get_device()
+
+    saturatedl = torch.ones(n,dtype=torch.float).to(device)*factorl
+    saturatedr = torch.ones(m,dtype=torch.float).to(device)*factorr
+
+    match = torch.zeros((n,m),dtype=torch.float).to(device)
+
+    for i in range(7,-3,-1):
+        level = -torch.pow(torch.tensor(4.0),torch.tensor(i)).to(device)
+        if i == -2:
+            level = torch.tensor(0, dtype=torch.float).to(device)
+    
+        
+        weight = torch.exp(level*torch.cdist(X, Y)*torch.cdist(X, Y))*saturatedr
+        # print(weight)
+
+        s = torch.sum(weight, axis=1)
+        s[s < 1e-9] = 1e-9
+
+        weight = weight/s[:,None]*saturatedl[:,None]
+        ss = torch.ones(n,dtype=torch.float).to(device)*1e-9
+        ss += torch.sum(weight, axis=0)
+        ss[ss < 1e-9] = 1e-9
+
+        ss = saturatedr/ss 
+        ss[ss>1.0] = 1.0
+
+        weight = weight*ss
+        s = torch.sum(weight, axis=0)
+        ss2 = torch.sum(weight, axis=0)
+
+        saturatedl = saturatedl-s
+        saturatedl[saturatedl < 0] = 0
+
+        match = match + weight
+
+        saturatedr = saturatedr - ss2
+        saturatedr[saturatedr < 0] = 0
+
+    return match
+
+
+def matchCost(X, Y, match):
+    """
+    Calculates Loss
+
+    Parameter : 
+    X (torch.tensor) : 3D point cloud of dimension (2048,3)
+    Y (torch.tensor) : 3D point cloud of dimension (2048,3)
+    (torch.tensor) : A matching matrix of dimension (2048, 2048) from
+                    approxMatch function.
+
+    Returns:
+    (int) : Earth Mover Distance 
+    """ 
+    
+    cost = (torch.cdist(X, Y)*match).sum()
+    return cost
+
+
+def EMD(X,Y):
+	"""
+	Calculates the Earth Mover Distance between two point clouds 
+	using Auction LAP
+
+	d_{EMD}(S_1, S_2) = \min_{\phi: S_1 \rightarrow S_2} \sum_{x \in S_1} ||x-\phi(x)|| \quad \textrm{where} \ \phi : S_1 \rightarrow S_2 \ \textrm{is bijection}
+
+	Step 1) Calculate the approximate match matrix between 3D point cloud
+	Step 2) Calculate the cost function based on match matrix
+
+	Parameter : 
+	X (torch.tensor) : 3D point cloud with dimension (2048,3)
+	Y (torch.tensor) : 3D point cloud with dimension (2048,3)
+
+	Returns:
+	(int) : Earth mover distance between 3D point clouds X and Y
+
+	Reference:
+	1) https://github.com/optas/latent_3d_points/blob/master/external/structural_losses/approxmatch.cpp
+	2) https://github.com/daerduoCarey/PyTorchEMD
+	
+	"""
+	mat = approxMatch(X, Y)
+	cost = matchCost(X, Y, mat)
+
+	return cost
+
+	
+def EMDBatch(X_batch, Y_batch):
+	"""
+	Earth Mover distance between two batches of pointcloud
+	"""
+	loss = [EMD(X_batch[i],Y_batch[i]) for i in range(X_batch.shape[0])]
+	return loss
